e776467c2411eb49d7dd71aab4bb4cf327f52d91
Ida <ihuangda@gmail.com>
2014-04-28 22:54:58 -0400

    beginning

---
+++ Jumper.java
@@ -0,0 +1,96 @@
+// Group Something
+// APCS Period 9 
+// HW33- GridWorld 3
+// 2014-04-25
+
+//Jumper Class
+
+package info.gridworld.actor;
+
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+
+/**
+ * A <code>Jumper</code> is an actor that can move forward two cells in each move . It jumps over rocks and flowers. It does not leave anything behind. 
+ */
+
+
+public class Jumper extends Actor
+{
+    /**
+     * Constructs a red Jumper.
+     */
+    public Jumper()
+    {
+        setColor(Color.RED);
+    }
+
+    /**
+     * Constructs a Jumper of a given color.
+     * @param jumperColor the color for this Jumper
+     */
+    public Jumper(Color jumperColor)
+    {
+        setColor(jumperColor);
+    }
+
+    /**
+     * Moves if it can move, turns otherwise.
+     */
+    public void act()
+    {
+        if (canMove())
+            move();
+        else
+            turn();
+    }
+
+    /**
+     * Turns the jumper 45 degrees to the right without changing its location.
+     */
+    public void turn()
+    {
+        setDirection(getDirection() + Location.HALF_RIGHT);
+    }
+
+    /**
+     * Moves the jumper forward, putting a flower into the location it previously
+     * occupied.
+     */
+    public void move()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (gr.isValid(next))
+            moveTo(next);
+        else
+            removeSelfFromGrid();
+        Flower flower = new Flower(getColor());
+        flower.putSelfInGrid(gr, loc);
+    }
+
+    /**
+     * Tests whether this jumper can move forward into a location that is empty or
+     * contains a flower.
+     * @return true if this jumper can move.
+     */
+    public boolean canMove()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next))
+            return false;
+        Actor neighbor = gr.get(next);
+        return (neighbor == null) || (neighbor instanceof Flower);
+        // ok to move into empty location or onto flower
+        // not ok to move onto any other actor
+    }
+}
